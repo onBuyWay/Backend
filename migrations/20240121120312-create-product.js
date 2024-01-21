86b9092e67eccd37aff7194a5a82e1e185c1177f
KevinHong71336099 <kevin71336099@gmail.com>
2024-01-21 20:18:54 +0800

    feat: add Product model

---
+++ migrations/20240121120312-create-product.js
@@ -0,0 +1,49 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      stock_quantity: {
+        type: Sequelize.INTEGER
+      },
+      cost_price: {
+        type: Sequelize.INTEGER
+      },
+      sell_price: {
+        type: Sequelize.INTEGER
+      },
+      product_status: {
+        type: Sequelize.STRING
+      },
+      category_id: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products')
+  }
+}
